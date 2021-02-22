ed06d71e01e3db38efde10e78b4bd3e1fe0d8293
Treesa Mary K J <66899628+treesamary999@users.noreply.github.com>
2021-02-22 10:49:52 +0530

    Update table.py

--- table.py
+++ table.py
@@ -1,28 +1,40 @@
-#finding top 5, bottom 5 and highest products in each area
-#Created by:Treesa Mary K J on:18-02-2021
-
 import pandas as pd
+import logging
 
-data = pd.read_excel (r'C:\Users\Riya\OneDrive\Documents\py\sales.xlsx')
-df=pd.DataFrame(data,columns=['Quantity','Product_ID'])
+def main():
+    logging.basicConfig(level=logging.INFO, format='%(message)s/////%(asctime)s')
+    global data
+    data = pd.read_excel (r'C:\Users\Riya\OneDrive\Documents\py\sales.xlsx')
+    global df
+    df=pd.DataFrame(data,columns=['Quantity','Product_ID'])
+    #calling both functions
+    top()
+    highest()
 
-#finding top 5 products
-sorted_descending=df.sort_values(by=['Quantity'],ascending=False)
-print"*****Top 5 Products******"
-print(sorted_descending.head(5))
-print('\n')
 
-#finding bottom 5 products
-print"*****Bottom 5 Products******"
-print(sorted_descending.tail(5))
-print('\n')
+#logging.info('This will get logged')
 
-#finding highest product in each area
-n=pd.DataFrame(data)
-#accessing product_ID,area and quantity
-product_area=n[['Product_ID','Quantity','Area']]
-#sorting quantity in descending order
-sorted=product_area.sort_values(by=['Quantity'],ascending=False)
-highest_product=sorted.groupby('Area')
-print"****Highest Product in Each Area*****"
-print sorted.iloc[[4,2,0]]
+def top():
+    global sorted_descending
+    sorted_descending=df.sort_values(by=['Quantity'],ascending=False)
+    logging.info("*****TOP 5 PRODUCTS******")
+    print(sorted_descending.head(5))
+    print('\n')
+    logging.info("*****BOTTOM 5 PRODUCTS******")
+    print(sorted_descending.tail(5))
+    print('\n')
+
+
+def highest():
+    n=pd.DataFrame(data)
+    #accessing product_ID,area and quantity
+    product_area=n[['Product_ID','Quantity','Area']]
+    #sorting quantity in descending order
+    sorted=product_area.sort_values(by=['Quantity'],ascending=False)
+    highest_product=sorted.groupby('Area')
+    logging.info("*****HIGHEST PRODUCT IN EACH AREA******")
+    print sorted.iloc[[4,2,0]]
+
+
+if __name__=='__main__':
+    main()
